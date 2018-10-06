1372e09e343eca8537eaa2c959c4b851a053e84b
Jimmy Bell <jimmybell82@gmail.com>
2018-10-05 17:37:01 -0700

    moved example for createAccount outside of index.html.

--- backend/public/javascript/accApp.js
+++ backend/public/javascript/accApp.js
@@ -2,13 +2,14 @@
 
 /* Function: createAccount()
 Description: this function handles the clicking of the submit button for creating account data.
-Values for id={user,password,...} are passed into the backend http://localhost:3000/login to be added
-to the database.  All buttons and fields need to have class="create_account" as an additional tag.
+Values for id={user,password,...} are passed into the backend http://localhost:3000/createAccount so an
+account may be added to the database.
+class="create_account" is required for buttons and fields to work properly.
 */
 function createAccount() {
     var user,pass;
-    $("#submit").click(function() {
-	user=$("#user.create_account").val();
+    $("#submit.create_account").click(function() {
+	user=$("#username.create_account").val();
 	pass=$("#password.create_account").val();
 	//.post is an asychronous function
 	$.post("http://localhost:3000/createAccount",{user: user,password: pass}, function(data) {
