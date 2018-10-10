d7744fdef50b983a3c13d2fcf6f3f2aaab49ba2a
nablec <leehomcheng@live.com>
2018-10-10 14:00:10 -0700

    add viewProfile

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var body_parser = require('body-parser');
 var createAccountRouter = require('./routes/createAccount');
+var viewProfileRouter = require('./routes/viewProfile');
 var logger = require('morgan');
 var app = express();
 
@@ -9,5 +10,6 @@ app.use(body_parser.json());
 app.use('/',express.static('./public'));
 app.use('/javascript',express.static('./public/javascript'));
 app.use('/createAccount',createAccountRouter);
+app.use('/viewProfile', viewProfileRouter);
 
 module.exports = app;
--- backend/dbConnection.js
+++ backend/dbConnection.js
@@ -1,15 +1,19 @@
 var mysql = require('mysql');
 
 var dbconnection = mysql.createConnection({
+    //host: "localhost",
+    //user: "cs561group",
+    //password: "Ih34RTAcc0unt1ng",
+    //database: "app"
     host: "localhost",
-    user: "cs561group",
-    password: "Ih34RTAcc0unt1ng",
+    user: "root",
+    password: "123456",
     database: "app"
 });
 
 dbconnection.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-})
+});
 
 module.exports = dbconnection;
