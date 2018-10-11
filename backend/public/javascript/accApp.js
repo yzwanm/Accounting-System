80b5d92e4de35f26b570a242b0c6eb1a632252bc
Jimmy Bell <jimmybell82@gmail.com>
2018-10-10 21:05:29 -0700

    updated javascript/accApp.js to change page when SUCCESS is returned from /createAccount

--- backend/public/javascript/accApp.js
+++ backend/public/javascript/accApp.js
@@ -4,28 +4,37 @@
 Description: this function handles the clicking of the submit button for creating account data.
 Values for id={user,password,...} are passed into the backend http://localhost:3000/createAccount so an
 account may be added to the database.
-class="create_account" is required for buttons and fields to work properly.
 */
 function createAccount() {
-    var user,pass;
-    $("#submit.create_account").click(function() {
-	user=$("#username.create_account").val();
-	pass=$("#password.create_account").val();
-	fName=$("#firstname.create_account").val();
-	lName=$("#lastname.create_account").val();
-	dob=$("#dob.create_account").val();
-	age=$("#age.create_account").val();
-	sex=$("#sex.create_account").val();
-	income=$("#income.create_account").val();
+    $("#submit").click(function() {
+	var user=$("#username").val();
+	var pass=$("#password").val();
+	var fName=$("#firstname").val();
+	var lName=$("#lastname").val();
+	var dob=$("#dob").val();
+	var age=$("#age").val();
+	var sex=$("#sex").val();
+	var income=$("#income").val();
 	$.post("http://localhost:3000/createAccount",{user: user,password: pass, first_name: fName, last_name: lName, dob: dob, age: age, sex:sex, income:income}, function(data) {
-
-	    
-	    //////////////////////////////
-	    /*replace with desired code */
-	    //////////////////////////////
-	    alert(data);
-
-	    
+	    if (data === "SUCCESS") {
+		//if the account was created successfully go to the following address
+		var url = "http://localhost:3000/login.html";
+		document.location.href = url;
+	    } else if (data === "ERR_USER_EXISTS")  {
+		//the user is already in the database
+		alert(user + " already exists");
+	    } else if (data === "ERR_NO_USER") {
+		//user is null or empty
+		alert("User field is required");
+	    } else if (data === "ERR_NO_PASS") {
+		//pass is null or empty
+		alert("password field is required");
+	    } else if (data === "ERR_NO_FNAME") {
+		//fName is null or empty
+		alert("First Name field is required");
+	    } else {
+		alert("an error has prevented you from signing up. (" + data + ")");
+	    }
 	}); 
     });
 };
