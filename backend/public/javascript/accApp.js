b88f0b9276a7a953f6c3fdd01d9b4ade8ac08949
Jimmy Bell <jimmybell82@gmail.com>
2018-10-13 08:45:30 -0700

    updated example script for using /createAccount

--- backend/public/javascript/accApp.js
+++ backend/public/javascript/accApp.js
@@ -6,39 +6,72 @@ Values for id={user,password,...} are passed into the backend http://localhost:3
 account may be added to the database.
 */
 function createAccount() {
-    var user,pass;
-    $("#submit.create_account").click(function() {
-	user=profile.username;
-	pass=$("#password.create_account").val();
-	fName=$("#firstname.create_account").val();
-	lName=$("#lastname.create_account").val();
-	dob=$("#dob.create_account").val();
-	age=$("#age.create_account").val();
-	sex=$("#sex.create_account").val();
-	income=$("#income.create_account").val();
-	$.post("http://localhost:3000/createAccount",{user: user,password: pass, first_name: fName, last_name: lName, dob: dob, age: age, sex:sex, income:income}, function(data) {
-	    if (data === "SUCCESS") {
-		//if the account was created successfully go to the following address
-		var url = "http://localhost:3000/login.html";
-		document.location.href = url;
-	    } else if (data === "ERR_USER_EXISTS")  {
-		//the user is already in the database
-		alert(user + " already exists");
-	    } else if (data === "ERR_NO_USER") {
-		//user is null or empty
-		alert("User field is required");
-	    } else if (data === "ERR_NO_PASS") {
-		//pass is null or empty
-		alert("password field is required");
-	    } else if (data === "ERR_NO_FNAME") {
-		//fName is null or empty
-		alert("First Name field is required");
-	    } else {
-		alert("an error has prevented you from signing up. (" + data + ")");
+    $("#submit").click(function(e) {
+	e.preventDefault();
+	var user=$("#username").val();
+	var pass=$("#password").val();
+	var fName=$("#firstname").val();
+	var lName=$("#lastname").val();
+	var dob=$("#dob").val();
+	var age=$("#age").val();
+	var sex=$("#sex").val();
+	var income=$("#income").val();
+	//loading form data to be passed to the server
+	var formData = new FormData();
+	formData.append("user",user);
+	formData.append("password",pass);
+	formData.append("first_name",fName);
+	formData.append("last_name",lName);
+	formData.append("dob",dob);
+	formData.append("age",age);
+	formData.append("sex",sex);
+	formData.append("income",income);
+	if (($("#fileSelector"))[0].files.length > 0) {
+	    //if there is a file we will place it in a variable
+	    //and then append it to the formData
+	    var photo = ($("#fileSelector"))[0].files[0];
+	    formData.append("picture",photo,photo.name);
+	}
+	$("#submit").val("proccessing...");
+	$.ajax({
+	    url: 'http://localhost:3000/createAccount',
+	    type: 'POST',
+	    data: formData,
+	    //async: false,
+	    //cache: false,
+	    contentType: false,
+	    processData: false,
+	    success: function (data) {
+		if (data === "SUCCESS") {
+		    //if the account was created successfully go to the following address
+		    var url = "http://localhost:3000/login.html";
+		    document.location.href = url;
+		} else if (data === "ERR_USER_EXISTS")  {
+		    //the user is already in the database
+		    $("#textreply").text(user + " already exists");
+		    $("#submit").val("Submit");
+		} else if (data === "ERR_NO_USER") {
+		    //user is null or empty
+		    $("#textreply").text("User field is required");
+		    $("#submit").val("Submit");
+		} else if (data === "ERR_NO_PASS") {
+		    //pass is null or empty
+		    $("#textreply").text("password field is required");
+		    $("#submit").val("Submit");
+		} else if (data === "ERR_NO_FNAME") {
+		    //fName is null or empty
+		    $("#textreply").text("First Name field is required");
+		    $("#submit").val("Submit");
+		} else {
+		    $("#textreply").text("an error has prevented you from signing up. (" + data + ")");
+		    $("#submit").val("Submit");
+		}
+	    },
+	    error: function () {
+		alert("error: ajax failed to submit");
 	    }
-	}); 
+	});
     });
 };
-$(document).ready(createAccount);
-
+$(document).ready(createAccount);  
 
