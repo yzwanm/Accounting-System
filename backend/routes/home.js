03482f400e446da467693c4928442bedda059de0
nablec <leehomcheng@live.com>
2018-11-01 19:20:47 -0700

    integrate frontend dir

--- backend/routes/home.js
+++ backend/routes/home.js
@@ -4,7 +4,6 @@ var router = express.Router();
 
 async function get_recent_transactions(user_name,elements,callback) {
     console.log('request received');
-    
     console.log(user_name);
     console.log(elements);
     var sql1 = "select USER_NAME,EXPENSES,CATEGORY from expenses where USER_NAME= ?";
@@ -13,10 +12,10 @@ async function get_recent_transactions(user_name,elements,callback) {
         dbconnection.query(sql1, [user_name], function (error, results) {
             if (error) {
               console.log("error ocurred", error);
-            } 
+            }
             jsond = JSON.stringify(results);
                         //   res.render('Home', {title:'Home', home:jsond});
-            resolve(jsond); 
+            resolve(jsond);
           });
 
     });
@@ -33,7 +32,7 @@ router.get('/', function(req, res) {
     get_recent_transactions(user_name,elements,function(result){
         res.send(result);
     });
-    
+
 });
 
 module.exports = router;
