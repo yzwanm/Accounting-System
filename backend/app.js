80527c98fda2daadade604010dcfe4eab2126de4
Jimmy Bell <jimmybell82@gmail.com>
2018-11-02 09:33:19 -0700

    implemented sessions for login and logout

--- backend/app.js
+++ backend/app.js
@@ -5,19 +5,36 @@ var body_parser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors')
+var session = require('express-session');
+var MemoryStore = require('memorystore')(session);
+var cors = require('cors');
+var uuidv4 = require('uuid/v4');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
+var logoutRouter = require('./routes/logout');
 var createAccountRouter = require('./routes/createAccount');
 var loginRouter = require('./routes/login');
 var viewProfileRouter = require('./routes/viewProfile');
 var homeRouter = require('./routes/home');
 
 var app = express();
-var cors = require('cors')
 
-app.use(cors())
+app.use(cookieParser("fF4S35DG5"));
+app.use(session(
+    {genid: function(req) {
+	return uuidv4(); // use UUIDs for session IDs
+    },
+    store: new MemoryStore({
+	checkPeriod: 86400000
+    }),
+    secret: "fF4S35DG5",
+    resave:false,
+    saveUninitialized:true}));
+app.use(cors({credentials: true,
+	      origin: 'http://localhost:8100'}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,12 +45,12 @@ app.use(express.json());
 app.use(body_parser.urlencoded({ extended: false}));
 app.use(body_parser.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login',loginRouter);
+app.use('/logout',logoutRouter);
 app.use('/createAccount',createAccountRouter);
 app.use('/viewProfile', viewProfileRouter);
 app.use('/home', homeRouter);
